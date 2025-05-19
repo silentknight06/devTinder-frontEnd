62984358450df91c3e1db0165eaf484f35919b53
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-19 16:31:12 +0530

    Connection Feature Added!

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from  "./userSlice.js";
 import feedReducer from  "./feedSlice.js";
+import connectionReducer from "./connectionSlice.js";
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
         feed: feedReducer,
+        connections:connectionReducer,
     },
 });
 export default appStore;
