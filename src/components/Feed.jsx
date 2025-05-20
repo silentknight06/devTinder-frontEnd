b480a85f27f0ff1490bbe288c56f94383b367b45
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-20 17:14:45 +0530

    Added Ignore/Interested feature in Our App

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -28,6 +28,9 @@ const Feed = () => {
     getFeed();
   }, []);
 
+  if(!feed)return;
+  if(feed.length<=0)return <h1 className="flex justify-center my-10 font-bold"> No User Found </h1>
+
   return (
      (
       (feed && <div className="flex justify-center my-10">
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utils/feedSlice";
 
-const UserCard = ({user}) => {
-    const { firstName, lastName, photoUrl, age, gender, skills, about}= user;
+const UserCard = ({ user }) => {
+  const dispatch = useDispatch();
+  const { _id,firstName, lastName, photoUrl, age, gender, about } = user;
+
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="card bg-base-300 rounded-md w-96 shadow-sm">
-  <figure>
-    <img
-      src={photoUrl}
-      alt="Photo" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{firstName + " " +lastName}</h2>
-   {age && gender && <p>{age + ", " + gender}</p>}
-   <p> {about} </p>
-    <div className="card-actions my-4 justify-center">
-      <button className="btn btn-secondary">Ignore </button>
-        <button className="btn btn-primary"> Interested  </button>
+      <figure>
+        <img src={photoUrl} alt="Photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
+        <p> {about} </p>
+        <div className="card-actions my-4 justify-center">
+          <button className="btn btn-secondary" onClick={()=>{
+            handleSendRequest("ignored", _id)
+          }}>Ignore </button>
+          <button className="btn btn-primary" onClick={()=>{
+            handleSendRequest("interested", _id)
+          }} > Interested </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
-export default UserCard
- 
+export default UserCard;
