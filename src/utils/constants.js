6b9b3742ea4798f984b477ac25593a5861d84794
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-23 11:53:12 +0530

    Change Backend Url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://devtinder-2crp.onrender.com"
